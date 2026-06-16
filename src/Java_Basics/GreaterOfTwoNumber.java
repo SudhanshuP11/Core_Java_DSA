7a2dd0b120215c77778e7b1c91390b2b90820574
SudhanshuP11 <connect.sudhanshupathak@gmail.com>
2026-06-16 22:39:53 +0530

    Add new

---
+++ src/Java_Basics/GreaterOfTwoNumber.java
@@ -0,0 +1,15 @@
+package Java_Basics;
+
+import java.util.Scanner;
+
+public class GreaterOfTwoNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int b = sc.nextInt();
+        if(a>b) System.out.println("First number is greater");
+        else System.out.println("Second number is greater");
+    }
+}
