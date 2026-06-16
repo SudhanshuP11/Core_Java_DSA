2878e4f1ed1eec5767366b68d642e0d72bbbc405
SudhanshuP11 <connect.sudhanshupathak@gmail.com>
2026-06-16 18:04:21 +0530

    Add new

---
+++ src/Java_Basics/EvenAndOdd.java
@@ -0,0 +1,13 @@
+package Java_Basics;
+
+import java.util.Scanner;
+
+public class EvenAndOdd {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check: ");
+        int number = sc.nextInt();
+        if(number%2==0) System.out.println("Given number is Even");
+        else System.out.println("Given number is odd");
+    }
+}
