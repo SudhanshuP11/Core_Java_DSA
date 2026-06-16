be8e5fcdff1b2c90e493c20934a235a5fb080770
SudhanshuP11 <connect.sudhanshupathak@gmail.com>
2026-06-16 18:16:23 +0530

    Add new

---
+++ src/Java_Basics/VotingEligibility.java
@@ -0,0 +1,13 @@
+package Java_Basics;
+
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age: ");
+        int age = sc.nextInt();
+        if(age>=18) System.out.println("Eligible");
+        else System.out.println("Not Eligible");
+    }
+}
