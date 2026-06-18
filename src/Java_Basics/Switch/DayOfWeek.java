6581fd7b1b62a01b7033d5614728c16179e1a0da
SudhanshuP11 <connect.sudhanshupathak@gmail.com>
2026-06-18 13:59:21 +0530

    Add new

--- src/Java_Basics/Switch/DayOfWeek.java
+++ src/Java_Basics/Switch/DayOfWeek.java
@@ -1,4 +1,21 @@
 package Java_Basics.Switch;
 
+import java.util.Scanner;
+
 public class DayOfWeek {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. to the day name: ");
+        int d = sc.nextInt();
+        switch (d){
+            case 1: System.out.println("Monday");break;
+            case 2: System.out.println("Tuesday");break;
+            case 3: System.out.println("Wednesday");break;
+            case 4: System.out.println("Thursday");break;
+            case 5: System.out.println("Friday");break;
+            case 6: System.out.println("Saturday");break;
+            case 7: System.out.println("Sunday");break;
+            default: System.out.println("Invalid day");break;
+        }
+    }
 }
