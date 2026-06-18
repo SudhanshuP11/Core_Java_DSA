a4163f58e9b2a0f02c366d1820cd99c8503b7263
SudhanshuP11 <connect.sudhanshupathak@gmail.com>
2026-06-18 14:53:40 +0530

    Add new

---
+++ src/Java_Basics/Patterns/SquarePatternPrint.java
@@ -0,0 +1,13 @@
+package Java_Basics.Patterns;
+
+public class SquarePatternPrint {
+    public static void main(String[] args){
+        System.out.println("Square Pattern: ");
+        for(int i =1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
