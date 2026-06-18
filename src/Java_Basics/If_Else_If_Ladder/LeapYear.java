14921e97faca7356a71538a26fc98776e232f7ed
SudhanshuP11 <connect.sudhanshupathak@gmail.com>
2026-06-18 14:21:18 +0530

    Add new

---
+++ src/Java_Basics/If_Else_If_Ladder/LeapYear.java
@@ -0,0 +1,15 @@
+package Java_Basics.If_Else_If_Ladder;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year: ");
+        int year = sc.nextInt();
+        if(year%400==0) System.out.println("Year is leap year");
+        else if (year%4==0&year%100!=0) System.out.println("Year is a leap year");
+        else System.out.println("Not a leap year");
+
+    }
+}
